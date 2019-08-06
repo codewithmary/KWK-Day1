48a1285291dac2e803a68984c8eb0cd488348649
Taylor <apple@Apples-MacBook-Pro-45.local>
2019-08-06 10:29:21 -0400

    Day 1

---
+++ 01datatypeandvaribleMyPlayground.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+var str = "Hello, playground"
+//puppies
+"1234"
+"Hello world"
+
+//Example of a variable
+var address = "10 hudson yard"
+address = "48th floor"
+var companyName : String
+companyName = "Best Company Ever"
+
+//Example of a constant
+var planet = "earth"
+planet = "venus"
+//examples of joining variables and string
+var string1 = "welcome"
+var string2 = "Mary Abreu"
+
+var Welcome1 = "\(string1) \(string2)"
+var Welcome2 = string1 + " " + string2
+
+print(Welcome1)
+print("Here is my sentence")
+
+var first = "Karlie"
+var last = "Kloss"
+
+print("\(first)\(last)")
+
+var first1 = "Karlie"
+var last2 = "Kloss"
+
+print("\(first)\(last)")
+//examples of integers
+1
+54
+-16
+
+//examples of floats and doubles
+0.13
+2.1
+3.14
+
+var integer = 4.0
+var double = 5.0
+
+
+4*4
+4*integer
+4*double
+integer*double
+
+
+//practice with numbers
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+
+(a+b+c+d)/4
+a+b*c/d
+
+//Part 1 variable of constant
+
+name = constant
+address = variable
+birthday = constant
+gradelevel = constant
+Age = variable
+eye color = constant
+favorite color = variable
+
+//Part 2: how much does she get paid?
+ var payday = 10.25*20
+
+//Part 3: Happy birthday
+var name = Julian
+
---
+++ MyPlayground.conditionals.playground/Contents.swift
@@ -0,0 +1,48 @@
+//import UIKit
+//
+//var string = "Hello, playground"
+//// Comparison Operators
+//
+//5<3
+//12>7
+//6 != 8
+////7=="7"
+//"karlie" == "karlie"
+//"karlie" == "karliekloss"
+//"Karlie" == "karlie"
+//
+//var luckynum = 7
+//luckynum < 10
+//luckynum == 7
+//
+////Examples of conditions
+//var dogAge = 2
+//if dogAge < 2 {
+//    print("you are a puppy!")
+//}
+//else if dogAge > 12 {
+//    print("you are awesome")
+//}
+////Try it: Control Flow
+//
+//    var favoriteFood = "pasta"
+//    if favoriteFood > "chipotle" {
+//        print("Chipote is not as good pasta")
+//}
+//if favoriteFood == "starbucks" {
+//    print("pasta is equally as good as starbucks")
+//    }
+
+//Practice with conditionals
+
+var hasAccount = "false"
+if hasAccount == "false" {
+        print("Let's get some information to create an account for you")
+}
+
+// Gradelevels
+var gradeLevel = 14
+if gradeLevel < 6 {
+        print("elementary")
+}
+
